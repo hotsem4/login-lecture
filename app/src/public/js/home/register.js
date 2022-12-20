915a21c565be0772e1e175b436b5f6afd6bcbae5
bumsu Choe <hotsem100@gmail.com>
2022-12-20 22:49:08 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,18 +1,23 @@
 'use strict';
 
 const id = document.querySelector('#id'),
+  name = document.querySelector('#name'),
   psword = document.querySelector('#psword'),
-  loginBtn = document.querySelector('#button');
+  confirmPsword = document.querySelector('#confirm-psword'),
+  registerBtn = document.querySelector('#button');
 
-loginBtn.addEventListener('click', login);
+registerBtn.addEventListener('click', register);
 console.log(id);
 
-function login() {
+function register() {
   const req = {
+    name: name.value,
     id: id.value,
     psword: psword.value,
+    confirmPsword: confirmPsword.value,
   };
-  fetch('./login', {
+
+  fetch('./register', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -22,12 +27,12 @@ function login() {
     .then((res) => res.json())
     .then((res) => {
       if (res.success) {
-        location.href = '/';
+        location.href = '/login';
       } else {
         alert(res.msg);
       }
     })
     .catch((error) => {
-      console.log('로그인 중 에러 발생');
+      console.log('회원가입 중 에러 발생');
     });
 }
