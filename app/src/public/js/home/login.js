6f48f1e47f26463783fc3f2a4f51d15d8b345136
최범수 <choebeomsu@choebeomsuui-MacBookPro.local>
2022-12-20 16:01:20 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,5 +12,11 @@ function login() {
     id: id.value,
     psword: psword.value,
   };
-  console.log(req);
+  fetch('./login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(req),
+  });
 }
