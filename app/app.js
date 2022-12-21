9a8c0731483d761ffb1bd06333313b103d625570
bumsu Choe <hotsem100@gmail.com>
2022-12-21 09:23:53 +0900

    AWS RDS와 연동 & dotenv로 환경변수 등록

--- app/app.js
+++ app/app.js
@@ -2,6 +2,9 @@
 // 모듈
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
 
 // 라우팅
--- app/bin/www.js
+++ app/bin/www.js
@@ -2,7 +2,7 @@
 
 const app = require('../app');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log('서버 가동');
